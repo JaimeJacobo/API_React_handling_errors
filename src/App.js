c9a8954c51f76ee5b37d4d77943a8a61659e66d3
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-02-05 12:39:28 +0100

    subiendo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    data: {},
+    isLoaded: false,
+    errorMessage: ''
+  }
+
+  fetchElements(){
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=Chicken_Alfredo_Primavera')
+    .then((response)=>{
+      return response.json();
+    })
+    .then((responseJSON)=>{  
+      if(responseJSON.meals == null){
+        this.setState({
+          errorMessage: 'There has been an error'
+        })
+      } else {
+        this.setState({
+          data: responseJSON,
+          isLoaded: true
+        })       
+      }
+    })
+  }
+
+  componentDidMount(){
+    this.fetchElements();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {
+          this.state.isLoaded
+          ? <h1>{this.state.data.meals[0].strMeal}</h1>
+          : <div>
+              <h1>Loading...</h1>
+              <h2>{this.state.errorMessage}</h2>
+            </div>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
